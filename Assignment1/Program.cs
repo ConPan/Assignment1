72900718a9712d744e0e4b9abb45765715089ffc
Constantin Pantesi <constantin.pantesi@yahoo.com>
2023-09-20 17:30:50 +0200

    Add project files.

---
+++ Assignment1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello user! Enter your name, please!");
+
+string userName = Console.ReadLine();
+if (userName != null) {
+    MyFirstMethod();
+}
+
+static void MyFirstMethod()
+{
+    Console.WriteLine($"Nice to meet you! Can you describe yourself in one sentence, please?");
+    string userInput = Console.ReadLine();
+    Console.WriteLine(userInput);
+}
+
---
+++ Assignment2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello user! Enter one word, please!");
+
+string userName = Console.ReadLine();
+if (userName != null)
+{
+    MyFirstMethod();
+}
+
+static void MyFirstMethod()
+{
+    Console.WriteLine($"Thank you! Could you enter some text, please?");
+    string userInput = Console.ReadLine();
+    Console.WriteLine("#" + userInput);
+}
